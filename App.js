8c381d2301cfbe98d3f95fa9abbe6394a2bd585c
sanjatinjak <tinjaksanja@gmail.com>
2021-03-19 15:21:39 +0100

    added navigation

--- App.js
+++ App.js
@@ -1,24 +1,40 @@
-import { View,StyleSheet } from 'react-native';
-import ListOfItems from "./presentation/Items/listOfItems";
+import 'react-native-gesture-handler';
+import { StyleSheet, Dimensions } from 'react-native';
+import HomeScreen from "./screens/Home";
+import LoginScreen from "./screens/Login";
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HeaderBar from './presentation/HeaderBar/Header';
+import CategoriesScreen from './screens/Categories';
 
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
+  
+    <NavigationContainer style={styles.container}>
+          <Stack.Navigator>
+            <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options = {{ title: "smth", header: props => <HeaderBar /> }}
+            />
+            <Stack.Screen 
+            name="Login" 
+            component={LoginScreen} />
+            <Stack.Screen 
+            name="Categories" 
+            component={CategoriesScreen} />
 
-    <View style={styles.container}>
-      <ListOfItems />
-    </View>
-
+          </Stack.Navigator>
+    </NavigationContainer>
+  
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    marginTop: 150,
-    paddingLeft: 20,
-    paddingRight: 20,
-    paddingBottom: 20
+    height: Dimensions.get('window').height
   },
 });
