0876fed1059fcfdd7d39e418fc9bd917b19c1f77
sanjatinjak <tinjaksanja@gmail.com>
2021-03-19 15:22:16 +0100

    created screens

---
+++ screens/Categories.js
@@ -0,0 +1,14 @@
+import { View,StyleSheet, Text } from 'react-native';
+import React from 'react';
+
+
+export default function CategoriesScreen() {
+  return (
+
+    <View>
+      <Text>CATEGORIES SCREEN</Text>
+    </View>
+
+  );
+}
+
---
+++ screens/Home.js
@@ -0,0 +1,16 @@
+import { View } from 'react-native';
+import ListOfItems from "../presentation/Items/listOfItems";
+import React from 'react';
+
+
+export default function HomeScreen() {
+  return (
+
+    <View>
+
+      <ListOfItems />
+    </View>
+
+  );
+}
+
